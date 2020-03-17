7a8ff7ab75221236e26ea26cc36f49362cf88de2
Bryce Fisher-Fleig <brycefisher@users.noreply.github.com>
2020-03-16 21:27:14 -0700

    feat!: Many 'rules' in config for separate commands

---
+++ src/config.rs
@@ -0,0 +1,54 @@
+use std::collections::HashMap;
+use std::fs;
+use std::path::{Path, PathBuf};
+
+use git2::{Repository, Oid, BranchType, Tree};
+use serde::Deserialize;
+
+use crate::Result;
+
+#[derive(Debug, Deserialize)]
+struct Rule {
+    command: String,
+    root: PathBuf,
+}
+
+#[serde(default)]
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    default_branch: String,
+    rules: HashMap<String, Rule>
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            default_branch: "master".into(),
+            rules: HashMap::new(),
+        }
+    }
+}
+
+impl Config {
+    pub fn load() -> Result<Config> {
+        match fs::read_to_string("lmfa0.toml") {
+            Ok(raw_toml) => toml::from_str(raw_toml.as_str()).map_err(From::from),
+            Err(_) => Ok(Config::default()),
+        }
+    }
+
+    pub fn base<'r>(&self, ref_file: &Path, repo: &'r Repository) -> Result<Tree<'r>> {
+        match fs::read_to_string(ref_file) {
+            Ok(base_ref) => {
+                let oid = Oid::from_str(&base_ref)?;
+                let commit = repo.find_commit(oid)?;
+                Ok(commit.tree()?)
+            }
+            Err(_) => {
+                let branch = repo.find_branch(&self.default_branch, BranchType::Local)?;
+                let r#ref = branch.into_reference();
+                Ok(r#ref.peel_to_tree()?)
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@
 //  5 - record a ref in a file listed in Config
 //  6 - Make the location of the repo configurable
 
-use std::fs;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
-use git2::{Repository, Oid, BranchType, DiffFile, Tree};
-use serde::Deserialize;
+use git2::{Repository, DiffFile};
+
+use config::Config;
+
+mod config;
 
 fn diff_file_in(ancestor: impl AsRef<Path>, diff_file: &DiffFile) -> bool {
     match diff_file.path() {
@@ -20,46 +22,6 @@ fn diff_file_in(ancestor: impl AsRef<Path>, diff_file: &DiffFile) -> bool {
 type Error = Box<dyn std::error::Error>;
 type Result<T> = std::result::Result<T, Error>;
 
-#[serde(default)]
-#[derive(Debug, PartialEq, Deserialize)]
-pub struct Config {
-    default_branch: String,
-    ref_file: PathBuf,
-}
-
-impl Default for Config {
-    fn default() -> Self {
-        Config {
-            default_branch: "master".into(),
-            ref_file: PathBuf::from(".lmfa0-ref"),
-        }
-    }
-}
-
-impl Config {
-    pub fn load() -> Result<Config> {
-        match fs::read_to_string("/etc/lmfa0/config.toml") {
-            Ok(raw_toml) => toml::from_str(raw_toml.as_str()).map_err(From::from),
-            Err(_) => Ok(Config::default()),
-        }
-    }
-
-    pub fn base<'r>(&self, repo: &'r Repository) -> Result<Tree<'r>> {
-        match fs::read_to_string(&self.ref_file) {
-            Ok(base_ref) => {
-                let oid = Oid::from_str(&base_ref)?;
-                let commit = repo.find_commit(oid)?;
-                Ok(commit.tree()?)
-            }
-            Err(_) => {
-                let branch = repo.find_branch(&self.default_branch, BranchType::Local)?;
-                let r#ref = branch.into_reference();
-                Ok(r#ref.peel_to_tree()?)
-            }
-        }
-    }
-}
-
 fn main() -> Result<()> {
     let repo = match Repository::open(".") {
         Ok(repo) => repo,
@@ -70,7 +32,7 @@ fn main() -> Result<()> {
     let config = Config::load()?;
 
     // TODO - figure out how to support nonbranch refs
-    let base_tree = config.base(&repo)?;
+    let base_tree = config.base(".lmfao".as_ref(), &repo)?;
 
     let diff = repo.diff_tree_to_workdir_with_index(Some(&base_tree), None)?;
 
