2f2655ee629b00da71d8f3cf5d95861f310fe7f7
Bryce Fisher-Fleig <brycefisher@users.noreply.github.com>
2020-05-06 21:09:19 -0700

    WIP: rejiggering signatures for dependency injection

--- src/azure_table.rs
+++ src/azure_table.rs
@@ -1,20 +1,29 @@
+use std::convert::TryInto;
 use git2::Oid;
 
 use crate::Result;
 
-#[derive(Debug, PartialEq)]
-pub struct TableClient {
+pub trait StorageTable {}
+
+pub struct Client<'c> {
     branch: String,
     job: String,
+    table: &'c dyn StorageTable,
 }
 
-impl TableClient {
-    /// Creates a TableClient scoped to a particular branch name and job.
-    pub fn new(branch: impl Into<String>, job: impl Into<String>) -> TableClient {
-        TableClient {
+impl<'c> Client<'c> {
+    /// Creates a StorageTableClient scoped to a particular branch name and job.
+    pub fn new(branch: impl Into<String>, job: impl Into<String>, table: impl TryInto<&'c dyn StorageTable, Error=crate::Error>) -> Result<Client<'c>> {
+        /*
+        let AzureTableConfig { account, sas, table } = config.azure_table_config()?;
+        let client = TableClient::azure_sas(&account, &sas)?;
+        let cloud_table = CloudTable::new(client, table);
+        */
+        Ok(Client {
             branch: branch.into(),
-            job: job.into()
-        }
+            job: job.into(),
+            table: table.try_into()?,
+        })
     }
 
     /// Fetches the Git SHA from Azure Storage Table
@@ -30,13 +39,4 @@ impl TableClient {
 
 #[cfg(test)]
 mod test {
-    use super::*;
-
-    #[test]
-    fn new_client() {
-        assert_eq!(
-            TableClient { branch: "master".into(), job: "test".into() },
-            TableClient::new("master", "test")
-        );
-    }
 }
